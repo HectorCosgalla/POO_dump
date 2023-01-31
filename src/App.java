c4589f420c1bcf7a77d23277b6fe4e866bac8efe
Hector Cosgalla <valcos98@hotmail.com>
2023-01-30 20:52:34 -0600

    Adding what i should add yesterday

--- src/App.java
+++ src/App.java
@@ -1,9 +1,28 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import database.Connection;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Connection database = new Connection();
+        java.sql.Connection database;
 
-        database.getConnection();
+        database = Connection.getConnection("u387570956_valcos98_Test","Zg=aTgwks[71","mysql","45.132.157.154","3306","u387570956_Test_connect");
+        String query = "SELECT * FROM student";
+        try (Statement statement = database.createStatement()){
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next()){
+                int studentId = resultSet.getInt("ID");
+                String name = resultSet.getString("name");
+                String department_name = resultSet.getString("dept_name");
+                int credits = resultSet.getInt("tot_cred");
+                System.out.println(studentId+"\t|\t"+name+"\t|\t"+department_name+"\t|\t"+credits);
+            }
+        } catch (SQLException e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+        database.close();
     }
 }
--- src/database/Connection.java
+++ src/database/Connection.java
@@ -5,33 +5,15 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Connection {
-
-    private String userName;
-    private String password;
-    private String dbms;
-    private String serverName;
-    private String portNumber;
-    private String dbName;
     
-    public Connection getConnection() throws SQLException {
-
-        Connection conn = null;
+    public static java.sql.Connection getConnection(String userName, String password, String dbms, String serverName, String portNumber, String dbName) throws SQLException {
+        java.sql.Connection conn = null;
         Properties connectionProps = new Properties();
-        connectionProps.put("user", this.userName);
-        connectionProps.put("password", this.password);
+        connectionProps.put("user", userName);
+        connectionProps.put("password", password);
     
-        if (this.dbms.equals("mysql")) {
-            conn =  (Connection) DriverManager.getConnection(
-                       "jdbc:" + this.dbms + "://" +
-                       this.serverName +
-                       ":" + this.portNumber + "/",
-                       connectionProps);
-        } else if (this.dbms.equals("derby")) {
-            conn = (Connection) DriverManager.getConnection(
-                       "jdbc:" + this.dbms + ":" +
-                       this.dbName +
-                       ";create=true",
-                       connectionProps);
+        if (dbms.equals("mysql")) {
+            conn =  DriverManager.getConnection("jdbc:" + dbms + "://" + serverName +":" + portNumber + "/"+dbName,connectionProps);
         }
         System.out.println("Connected to database");
         return conn;
